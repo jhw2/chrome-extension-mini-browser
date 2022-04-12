f16f16bc902206f1a1f299c104d68e283a0e35b0
jhw <hope5107@naver.com>
2022-04-12 22:31:51 +0900

    새로고침 시 미니 팝업 기존 페이지 유지

--- assets/dist/js/content.js
+++ assets/dist/js/content.js
@@ -1,4 +1,4 @@
-function setUserAgent(window, userAgent) {
+const setUserAgent = (window, userAgent) => {
     if (window.navigator.userAgent != userAgent) {
         var userAgentProp = { get: function () { return userAgent; } };
         try {
@@ -11,14 +11,34 @@ function setUserAgent(window, userAgent) {
     }
 }
 const goToSite = (e) => {
-    console.log(e.keyCode);
     if (e.keyCode === 13) {
-        //setUserAgent(document.querySelector('#mini-b-pop iframe').contentWindow, 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H)');
-        document.querySelector('#mini-b-pop iframe').src = e.target.value;
+        const url = e.target.value.includes('http://') || e.target.value.includes('https://') ? e.target.value : 'https://' + e.target.value;
+        document.querySelector('#mini-b-pop iframe').src = url;
     }
 }
+let saveUrl = "https://m.naver.com/";
+chrome.storage.local.get(['mini_b_url'], (result) => {
+    console.log('test',result);
+    if ( result.mini_b_url ) saveUrl = result.mini_b_url;
+});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request.current_url)
+    if (document.querySelector('#mini-b-pop input')) document.querySelector('#mini-b-pop input').value = request.current_url;
+});
 window.addEventListener('DOMContentLoaded', () => {
-    if (!document.querySelector("#mini-b-pop")) {
+    if ( self !== top ) {
+        window.navigator.__defineGetter__('userAgent', function () {
+            return 'Mozilla/5.0 (iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B206 CUSTOM';
+        });
+        console.log('load', location.href)
+        chrome.storage.local.set({mini_b_url: location.href}, () => {});
+        chrome.runtime.sendMessage({current_url: location.href}, (response) => { });
+        document.querySelectorAll('[target="_blank"]').forEach((e)=>{
+            e.removeAttribute('target')
+        });
+    }
+
+    if (self === top && !document.querySelector("#mini-b-pop")) {
         const div = document.createElement('div');
         div.id = 'mini-b-pop';
 
@@ -27,13 +47,11 @@ window.addEventListener('DOMContentLoaded', () => {
         urlInput.onkeydown = goToSite;
 
         const button = document.createElement('button');
-
         const iframe = document.createElement('iframe');
-        iframe.src = "https://m.naver.com/";
+        iframe.src = saveUrl;
 
         div.appendChild(urlInput);
         div.appendChild(iframe);
         document.body.appendChild(div);
-        setUserAgent(document.querySelector('#mini-b-pop iframe').contentWindow, 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H)');
     }
 });
